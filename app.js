43cd4268a9fcee206889652d444c731f6608b1d4
dannywo <dannywo@gmail.com>
2020-03-19 01:08:28 -0700

    create dom elements with api data then append movie and description to elements

--- app.js
+++ app.js
@@ -15,6 +15,28 @@ request.onload = function() {
 
         data.forEach(movie => {
             console.log(movie.title);
+            console.log(movie.description);
+            // Create a div with a card class
+            const card = document.createElement('div');
+            card.setAttribute('class', 'card');
+
+            // Create an h1 and set the text content to the film's title
+            const h1 = document.createElement('h1');
+            h1.textContent = movie.title;
+            // Create a p and set the text content to the film's description
+            const p = document.createElement('p');
+            // Limit description to 300 chars
+            movie.description = movie.description.substring(0, 300);
+            p.textContent = `${movie.description}...`;
+
+            // Append the cards to the container element
+            container.appendChild(card);
+
+            // Each card will contain an h1 and a p
+            card.appendChild(h1);
+            card.appendChild(p);
+
+
         });
     } else {
         console.log('error!!!!')
@@ -23,5 +45,18 @@ request.onload = function() {
 
 // Send request
 request.send();
+
+
+const app = document.querySelector("#root");
+ console.log(app);
+const logo = document.createElement('img');
+
+logo.src = 'logo.png';
+
+const container = document.createElement('div');
+container.setAttribute('class', 'container');
+
+app.appendChild(logo);
+app.appendChild(container);
 
 console.log("end app.js")
