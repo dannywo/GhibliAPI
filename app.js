7c3ce590edfb94ed3ed8faa7e2ec64da40c1e229
dannywo <dannywo@gmail.com>
2020-03-19 00:36:38 -0700

    pass test connecting to  api calls

--- app.js
+++ app.js
@@ -4,11 +4,21 @@ console.log("start app.js");
 let request = new XMLHttpRequest();
 
 // Open a new connection, using the GET request on the URL endpoint
-request.open('GET', 'https://ghibli.herokuapp.com/films', true);
+request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
 
 request.onload = function() {
     //Begin accessing JSON data here
+    let data = JSON.parse(this.response);
 
+    if (request.status >= 200 && request.status < 400) {
+        console.log(data);
+
+        data.forEach(movie => {
+            console.log(movie.title);
+        });
+    } else {
+        console.log('error!!!!')
+    }
 }
 
 // Send request
